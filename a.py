1cb9982767351c9ddce51022427281c748294215
Shriram <2015shriram.rajaraman@ves.ac.in>
2019-07-16 20:17:01 +0530

    First commit

---
+++ a.py
@@ -0,0 +1,24 @@
+
+import boto3
+import os
+import sys
+import uuid
+from PIL import Image
+import PIL.Image
+
+s3_client = boto3.client('s3')
+
+def resize_image(image_path, resized_path):
+    img = Image.open(image_path)
+    img = image.resize((200,200), PIL.Image.ANTIALIAS)
+    img.save(resized_path)
+
+def handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key'] 
+        download_path = '/{}{}'.format(uuid.uuid4(), key)                          
+        s3_client.download_file(bucket, key, download_path)
+        resize_image(download_path, upload_path)
+        s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+
